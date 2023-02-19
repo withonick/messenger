c63070ca2bd0d16554c1af95112f717eec6bb85b
Sultanov Otkirbek <stilinskij24@gmail.com>
2023-02-19 17:19:14 +0600

    baoj3

--- messenger/views.py
+++ messenger/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, "index.html")
 
+def about(request):
+    return render(request, "about.html")
+
 
 def page_not_found_view(request, exception):
     return render(request, 'errs/404.html', status=404)
--- network/urls.py
+++ network/urls.py
@@ -4,6 +4,7 @@ from messenger import views
 
 urlpatterns = [
     path("", views.index, name="home"),
+    path("about/", views.about, name="about"),
     path("admin/", admin.site.urls)
 ]
 
